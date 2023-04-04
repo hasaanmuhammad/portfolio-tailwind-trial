f217b2087e0b9be32226355669efe3db2e3ea662
hasaanmuhammad <ykhasan92@gmail.com>
2023-04-04 14:47:39 +0700

    portfolio tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#14b8a6',
+        dark: '#0f172a',
+        secondary: '#64748b',
+        tema: 'rgb(var(--warna-tema) / <alpha-value>)',
+      },
+      screens: {
+        '2xl': '1320px'
+      },
+      fontFamily: {
+        inter: ['Inter'],
+      },
+    },
+  },
+  plugins: [],
+}
